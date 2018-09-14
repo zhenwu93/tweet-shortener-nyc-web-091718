3b57bb39a7138bb6ab622f34f38b4b1481038505
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 05:08:48 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -37,3 +37,12 @@ if tweet.length > 140
    tweet
  end
 end
+
+def shortened_tweet_truncator(tweet)
+  tweet = selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    tweet[0..136] + "..."
+  else
+    tweet
+  end
+end
