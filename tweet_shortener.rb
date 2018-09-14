8a7458cdf88f0bce2c870a30e1e9b36fb11aae03
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 04:41:26 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,47 @@
-# Write your code here.
+def dictionary
+dictionary = {
+  "too" => "2",
+  "to" => "2",
+  "two" =>"2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
+
+def word_substituter(tweet)
+  words.each do |word|
+    string.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else 
+      word 
+    end 
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    tweet = word_substituter(tweet)
+    puts "#{tweet}"
+  end 
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet = selective_tweet_shortener(tweet)
+  if tweet.length > 140 
+    tweet[0..136] + "..."
+  else
+    tweet
+  end
+end
