7ebb735b2a9da4ff4a2a224a16f3b9f8b763716c
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 05:05:34 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -30,9 +30,10 @@ def bulk_tweet_shortener(tweets)
   end
 end
 
-selective_tweet_shortener(tweet_one)
-if tweet_one.length > 140
-  word_substituter(tweet_one)
+selective_tweet_shortener(tweet)
+if tweet.length > 140
+  word_substituter(tweet)
 else
-  tweet_one
-    end
+  tweet 
+  end
+end
