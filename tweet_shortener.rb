7043f8ed7f6c66bb9e66a4acf1eae7ac624301eb
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 04:51:28 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,14 +1,16 @@
-dictionary = {
-  "too" => "2",
-  "to" => "2",
-  "two" =>"2",
-  "four" => "4",
-  "for" => "4",
-  "be" => "b",
-  "you" => "u",
-  "at" => "@",
-  "and" => "&"
-}
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+   }
 end
 
 def word_substituter(tweet)
